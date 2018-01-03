d3f61d890ead6c5ac5b69f7d0ce28a9909aad13b
曾佑森 <2356281422@qq.com>
2018-01-03 22:25:00 +0800

    滚动加载（？？？）

--- music/src/containers/comments/detailComment.js
+++ music/src/containers/comments/detailComment.js
@@ -7,34 +7,56 @@ export default class DetailComment extends React.Component{
         super(props);
         this.state = {
             comments: this.props.comments,
-            id: this.props.id,
-            offset: 0
+            hotComments: this.props.comments.hotComments,
+            commentsList: this.props.comments.comments,
+            id: this.props.id, /*音乐id*/
+            offset: 0,  /*评论加载页数*/
+            isobtain: true /*是否允许继续加载*/
         }
     }
 
-    // getComment(){
-    //     FetchData('type=comments&id='+)
-    // }
+    getComment(){
+        if(this.state.isobtain){
+            FetchData('type=comments&id='+this.state.id + '&offset=' + this.state.offset,'get').then(res => {
+                this.setState({
+                    isobtain: !this.state.isobtain
+                })
+                res.json().then(response => {
+                    this.setState({
+                        commentsList: this.state.commentsList.concat(response.comments),
+                        isobtain: !this.state.isobtain
+                    })
+                })
+            })
+        }
+    }
 
     componentDidUpdate(){
-        let target = this.refs.comment_list;
-        let style = window.getComputedStyle(target,null) || target.currentStyle;
-        if(style.height !== 'auto'){
+        window.addEventListener('scroll',() => {
+            let target = this.refs.comment_list;
+            let style = window.getComputedStyle(target,null) || target.currentStyle;
             let height = +style.height.split('px')[0];
-            console.log(target);
-            window.addEventListener('scroll',() => {
-                let scrollTop = window.scrollTop;
+            if(style.height !== 'auto'){
+                let viewHeight = document.body.clientHeight;
+                let scrollTop = document.documentElement.scrollTop;
+                if(height - scrollTop - viewHeight   < 50 ){
+                    // 发起请求
+                    this.setState({
+                        offset: this.state.offset + 1
+                    },() => {
+                        this.getComment();
+                    })
+                }
+                console.log(height);
                 console.log(scrollTop);
-            });
-            console.log(height);
-            console.log(style.height);
-            console.log(this.state);
-        }
+                console.log(viewHeight);
+            }
+        });
+
     }
 
     render(){
         let comment = this.state.comments;
-        console.log(comment);
         return(
             <div className="comment_list" ref="comment_list">
                 <div className="header">
@@ -44,7 +66,7 @@ export default class DetailComment extends React.Component{
                 <p className="hot_comment_title">精彩评论</p>
                 <ul className="hot_comment">
                     {
-                        comment.hotComments.map((item,index) => {
+                        this.state.hotComments.map((item,index) => {
                             return <SingleComment info={item} key={index}/>
                         })
                     }
@@ -52,7 +74,7 @@ export default class DetailComment extends React.Component{
                 <p className="new_comment">最新评论</p>
                 <ul className="all_comment">
                     {
-                        comment.comments.map((item,index) => {
+                        this.state.commentsList.map((item,index) => {
                             return <SingleComment info={item} key={index}/>
                         })
                     }
