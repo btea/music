c423f25507ebffc8cd17b52cf2a6a1ee5fa06474
曾佑森 <2356281422@qq.com>
2018-01-06 23:09:38 +0800

    mv展示界面,index------

--- music/src/containers/mv/mvIndex.js
+++ music/src/containers/mv/mvIndex.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FetchData from '../../fetch/fetch';
+import './mvindex.css';
 
 
 export default class MV extends React.Component{
@@ -28,7 +29,28 @@ export default class MV extends React.Component{
         let info = this.state.mvinfo;
         if(info) {
             return (
-                <video src={info.brs[240]} controls width="100%" height="200"></video>
+                <div className="mv_info">
+                    <div className="mv_index">
+                        <div className="cover_img">
+                            <img src={info.cover} alt=""/>
+                        </div>
+                        <video src={info.brs[240]}  width="100%" height="235"></video>
+                        <div className="progress"></div>
+                    </div>
+                    <div className="name_time_list">
+                        <div className="mv_name">
+                            <span>MV</span>
+                            <span>{info.name}</span>
+                        </div>
+                        <div className="singer">歌手: {nameList(info.artists)}</div>
+                        <div className="time_play">
+                            <span className="time">发布: {info.publishTime}</span>
+                            |
+                            <span className="play">播放: {playCOunt(info.playCount)}</span>
+                        </div>
+                    </div>
+                </div>
+
             )
         }else{
             return(
@@ -38,4 +60,24 @@ export default class MV extends React.Component{
             )
         }
     }
+}
+function playCOunt(count){
+    let counts = '';
+    if(count < 10000){
+        counts = count;
+    }else if(count >= 10000 && count < 100000){
+        counts = (count / 10000).toFixed(1) + '万';
+    }else{
+        counts = Math.floor(count / 10000) + '万';
+    }
+    return counts;
+}
+
+function nameList(artist){
+    console.log(artist);
+    let artists = '';
+    for(let i = 0; i < artist.length; i++){
+        artists += artist[i].name + '/'
+    }
+    return artists.substr(0,artists.length - 1);
 }
