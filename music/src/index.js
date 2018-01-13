cca5bd7bba3e55c961080f62235f2681903951c1
web前端-曾佑森 <2356281422@qq.com>
2018-01-13 10:20:33 +0800

    registerServiceWorker作用注解

--- music/src/index.js
+++ music/src/index.js
@@ -34,8 +34,19 @@ class Index extends React.Component{
 
 ReactDOM.render(<Index />, document.getElementById('root'));
 
-// 作用？
 // registerServiceWorker();
+// 作用？  主要用于在身产环境中为用户在本地创创建一个service worker来缓存资源到本地，提升应用的访问速度
+// In production, we register a service worker to serve assets from local cache.
+
+// This lets the app load faster on subsequent visits in production, and gives
+// it offline capabilities. However, it also means that developers (and users)
+// will only see deployed updates on the "N+1" visit to a page, since previously
+// cached resources are updated in the background.
+
+// To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
+// This link also includes instructions on opting out of this behavior.
+
+
 // https://api.imjad.cn/cloudmusic/?type=search&search_type=1002&s=木暮睦    //获取个人信息
 // 我的歌单id集合
 // [
